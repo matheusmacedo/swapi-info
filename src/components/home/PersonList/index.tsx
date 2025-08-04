074555437bb10264a9079a2f33dcd3440c4a072f
Matheus Macedo <mmanganelidemacedo@gmail.com>
2025-08-04 15:00:36 -0700

    add get favorites button

--- src/components/home/PersonList/index.tsx
+++ src/components/home/PersonList/index.tsx
@@ -2,9 +2,14 @@ import type { Person } from "@/types/Person";
 import PersonCard from "../../person/PersonCard";
 import { useState } from "react";
 import { AnimatePresence, motion } from "motion/react";
+import { useLocalStorage } from "usehooks-ts";
 
 function PersonList({ data }: { data: Person[] }) {
   const [filteredData, setFilteredData] = useState(data);
+  const [favoritePeople, setFavoritePeople] = useLocalStorage<string[]>(
+    "favoritePeople",
+    []
+  );
 
   const handleSearch = (search: string) => {
     const filteredData = data.filter((person) =>
@@ -13,9 +18,16 @@ function PersonList({ data }: { data: Person[] }) {
     setFilteredData(filteredData);
   };
 
+  const handleFavoriteFilter = () => {
+    const filteredData = data.filter((person) =>
+      favoritePeople.includes(person.name)
+    );
+    setFilteredData(filteredData);
+  };
+
   return (
     <div className="h-screen flex flex-col min-w-full">
-      <div className="w-full p-4 sticky top-0 z-10">
+      <div className="w-full p-4 sticky top-0 z-10 flex flex-row gap-2">
         <input
           type="text"
           minLength={3}
@@ -24,6 +36,16 @@ function PersonList({ data }: { data: Person[] }) {
           onChange={(e) => handleSearch(e.target.value)}
           className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-accent w-full"
         />
+
+        <div>
+          <button
+            className="text-sm w-full font-bold whitespace-nowrap uppercase bg-accent p-2 rounded-lg h-full text-background"
+            type="button"
+            onClick={handleFavoriteFilter}
+          >
+            get favorites
+          </button>
+        </div>
       </div>
 
       <AnimatePresence mode="sync">
