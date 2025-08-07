030f378f33d98226841fc832bc1182d8149c82c4
Matheus Macedo <mmanganelidemacedo@gmail.com>
2025-08-06 18:40:06 -0700

    style: style changes to search

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,15 +5,13 @@ import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 export const Route = createRootRoute({
   component: () => (
     <>
-      <header className="fixed top-0 right-0 bg-background flex items-center justify-between w-full p-4">
+      <header className="bg-background flex items-center justify-between w-full p-4">
         <Link to="/">
           <h1 className="text-2xl font-bold text-text">Star Wars Characters</h1>
         </Link>
         <TeamToggle />
       </header>
-      <div className="mt-20">
-        <Outlet />
-      </div>
+      <Outlet />
       <TanStackRouterDevtools />
     </>
   ),
